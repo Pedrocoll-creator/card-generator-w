6406eda976ff3ebb983d3527b836a34b132c1251
Pedrocoll-creator <collpayeraspedro@gmail.com>
2025-03-10 20:27:42 +0000

    2

--- src/app.js
+++ src/app.js
@@ -1,11 +1,43 @@
-import "bootstrap";
-import "./style.css";
+function recargarPagina() {
 
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let recargarPagina
+        
+  location.reload();
+}
+window.onload = function () {
+  generateCard();
 };
+
+
+function getRandomFromArray(values) {
+  let randomIndex = Math.floor(Math.random() * values.length);
+  return values[randomIndex];
+}
+
+
+function generateCard() {
+  let figuras = ["♦", "♥", "♠", "♣"];
+  let valores = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+
+  let figura = getRandomFromArray(figuras);
+  let valor = getRandomFromArray(valores);
+
+
+  let topIcon = document.querySelector(".figura");
+  let bottomIcon = document.querySelector(".figura.invertido");
+  let cardValue = document.querySelector(".valor");
+
+
+  topIcon.innerHTML = figura;
+  bottomIcon.innerHTML = figura;
+  cardValue.innerHTML = valor;
+
+  
+  if (figura === "♦" || figura === "♥") {
+    topIcon.style.color = "red";
+    bottomIcon.style.color = "red";
+  } else {
+    topIcon.style.color = "black";
+    bottomIcon.style.color = "black";
+  }
+}
